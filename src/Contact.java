f1ce5e443900e6e94e5bdb5ad129262ccd363ecf
Sadam Parvez <Sadamparvez008@gmail.com>
2021-04-21 22:26:33 +0530

    Checkpoint 4 passed

--- src/Contact.java
+++
@@ -1,9 +0,0 @@
-public class Contact {
-    private  String name, phone, email;
-
-    public Contact(String name, String phone, String email) {
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-    }
-}
--- src/RegularTicket.java
+++ src/RegularTicket.java
@@ -1,5 +1,11 @@
-public class RegularTicket {
-    private    String specialServices;
+public class RegularTicket extends Ticket{
+    private  String specialServices;
+
+    public RegularTicket(String specialServices,String pnr, String from, String to, String departureDateTime, String arraivalDateTime, String seatNo, Flight flight, Passenger passenger, float price, boolean cancelled) {
+        super(pnr, from, to, departureDateTime, arraivalDateTime, seatNo, flight, passenger, price, cancelled);
+        this.specialServices=specialServices;
+    }
+
 
 
     public  String getSpecialServices(){
@@ -11,3 +17,4 @@ public class RegularTicket {
     }
 
 }
+
